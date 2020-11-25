a58c526fef84b264c24e2d1cb492b5df5264218f
Mirza Atif Baig <65837135+Mirza62@users.noreply.github.com>
2020-11-25 14:52:02 +0530

    Single Regularity Principle Implemented
    A software Module should have only one reason to change.
    high Cohesive and loose coupling is preferred.

---
+++ ArdalisRating/ConsoleLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace ArdalisRating
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+    }
+}
