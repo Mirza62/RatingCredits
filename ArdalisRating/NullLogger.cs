32adf69ab07705a9a52da24b5420847a38bdfa46
Mirza Atif Baig <65837135+Mirza62@users.noreply.github.com>
2020-11-25 19:27:59 +0530

    Dependency Inversion Principle
    High-level modules should not depend on low -level modules.Both should depend on abstractions
    -Abstarctions should not depend on details.
    -Details should depend on abstractions.

---
+++ ArdalisRating/NullLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class NullLogger : ILogger
+    {
+        public void Log(string message)
+        {
+
+        }
+    }
+}
