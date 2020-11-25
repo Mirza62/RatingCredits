1070bbef17594c4fe7828401f6c0661602554bae
Mirza Atif Baig <65837135+Mirza62@users.noreply.github.com>
2020-11-25 15:20:58 +0530

    Open/Closed Principle
    A Software Module should be closed for modification and open for extensions.

---
+++ ArdalisRating/AutoPolicyRater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class AutoPolicyRater : Rater
+    {
+        public AutoPolicyRater(RatingEngine engine, ConsoleLogger logger)
+            :base(engine,logger)
+        {
+        }
+
+        public override void Rate(Policy policy)
+        {
+            Logger.Log("Rating AUTO policy...");
+            Logger.Log("Validating policy.");
+            if (String.IsNullOrEmpty(policy.Make))
+            {
+                Logger.Log("Auto policy must specify Make");
+                return;
+            }
+            if (policy.Make == "BMW")
+            {
+                if (policy.Deductible < 500)
+                {
+                    Rating = 1000m;
+                }
+                Rating = 900m;
+            }
+        }
+    }
+}
