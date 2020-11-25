b80da6cda45b24f9c4fddbbd7d1c6bb82d6cc075
Mirza Atif Baig <65837135+Mirza62@users.noreply.github.com>
2020-11-25 15:37:55 +0530

    Liskov Substitution Principle
    IS-A Relationship ia insufficient between models and should be Replaced with IS-SUBSTITUTABLE-FOR
    LSP->Violation to be looked for
    1.Type checking
    2.Null Checking
    3.Throw not Implemented Exception

--- ArdalisRating/AutoPolicyRater.cs
+++ ArdalisRating/AutoPolicyRater.cs
@@ -6,27 +6,27 @@ namespace ArdalisRating
 {
     public class AutoPolicyRater : Rater
     {
-        public AutoPolicyRater(RatingEngine engine, ConsoleLogger logger)
-            :base(engine,logger)
+        public AutoPolicyRater(IRatingContext context)
+            : base(context)
         {
         }
 
         public override void Rate(Policy policy)
         {
-            Logger.Log("Rating AUTO policy...");
-            Logger.Log("Validating policy.");
+            _logger.Log("Rating AUTO policy...");
+            _logger.Log("Validating policy.");
             if (String.IsNullOrEmpty(policy.Make))
             {
-                Logger.Log("Auto policy must specify Make");
+                _logger.Log("Auto policy must specify Make");
                 return;
             }
             if (policy.Make == "BMW")
             {
                 if (policy.Deductible < 500)
                 {
-                    Rating = 1000m;
+                    _context.UpdateRating(1000m);
                 }
-                Rating = 900m;
+                _context.UpdateRating(900m);
             }
         }
     }
--- ArdalisRating/FloodPolicyRater.cs
+++ ArdalisRating/FloodPolicyRater.cs
@@ -6,8 +6,8 @@ namespace ArdalisRating
 {
     public class FloodPolicyRater : Rater
     {
-        public FloodPolicyRater(RatingEngine engine, ConsoleLogger logger)
-            :base(engine,logger)
+        public FloodPolicyRater(IRatingContext context)
+            : base(context)
         {
         }
 
@@ -43,8 +43,7 @@ namespace ArdalisRating
             {
                 multiple = 1.1m;
             }
-            _engine.Rating = policy.BondAmount * 0.05m * multiple;
+            _context.UpdateRating(policy.BondAmount * 0.05m * multiple);
         }
-
     }
 }
